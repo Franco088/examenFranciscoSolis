1e0839b0cc904a792b8d8d5b0e8289511794e137
franco088 <franciscosolis.pinilla@gmail.com>
2018-03-14 10:45:15 +0100

    examen

---
+++ src/examenED/CadenasTest.java
@@ -0,0 +1,79 @@
+package examenED;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadenasTest {
+
+	@Test
+	public void testQuitarAenPrimeras2Posiciones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSonPrimerosYultimos2CharsLosMismos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
